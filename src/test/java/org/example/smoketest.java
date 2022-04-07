37cccf964a9f8a3aacca6602e71694f53db85ef5
sagarmopagar <sagarmopagar@gmail.com>
2022-04-07 19:59:53 +0530

    smoketest.java file added

---
+++ src/test/java/org/example/smoketest.java
@@ -0,0 +1,16 @@
+package org.example;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class smoketest {
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+        System.out.println("hello");
+    }
+}
