bd87c342f75dcbee9479b89035e78972a581261c
sagarmopagar <sagarmopagar@gmail.com>
2022-04-08 11:25:16 +0530

    unit-tests added in feature 3

---
+++ src/test/java/org/example/unittest.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class unittest {
+
+    /**
+     * Adding a second test
+     */
+    @Test
+    public void test2() {
+
+        System.out.println("Second test");
+    }
+}
