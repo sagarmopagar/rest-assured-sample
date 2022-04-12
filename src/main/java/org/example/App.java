b0292f1738f1903016f22fda9cb9fa10a8581778
sagarmopagar <sagarmopagar@gmail.com>
2022-04-12 23:49:46 +0530

    rest assured sample

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,5 +1,9 @@
 package org.example;
 
+import io.restassured.RestAssured;
+
+import static io.restassured.RestAssured.*;
+
 /**
  * Hello world!
  *
@@ -8,6 +12,13 @@ public class App
 {
     public static void main( String[] args )
     {
+
+        baseURI = "https://jsonplaceholder.typicode.com";
+        given().header("Content-Type", "application/json").when().get("todos/1").then().log().all().statusCode(200);
         System.out.println( "Hello World!" );
+
+
+
+
     }
 }
